1e5d3d60ed9d7ae26a0b86c3f74e95118965e291
Usmaan Ali <usmaanalii@hotmail.co.uk>
2017-03-15 18:40:54 +0000

    Added login form check
    - Check if fields are empty
    - If empty, redirects to appropriate error page

--- components/headers/loggedin-header.php
+++ components/headers/loggedin-header.php
@@ -1,5 +1,6 @@
 <ul id="header">
     <li><a href="/code/tvitter/pages/profile-page.php">Profile</a></li>
+    <li><a href="/code/tvitter/pages/listusers-page.php">Users</a></li>
     <li><a href="/code/tvitter/pages/timeline-page.php">Timeline</a></li>
     <li><a href="/code/tvitter/pages/login-page.php">Logout</a></li>
 </ul>
--- login-logic.php
+++ login-logic.php
@@ -1,18 +1,27 @@
 <?php
     require_once 'includes/login.inc.php';
 
+    // check if username and password have been added
+    $complete_form = !empty($_POST['username']) && !empty($_POST['password']);
+
     $login_user = new UserLogin($_POST['username'], $_POST['password']);
 
     $password_match = $login_user->check_password_match();
 
-    if ($password_match) {
-        session_start();
-        $_SESSION['id'] = $login_user->login_session_variables()["id"];
-        $_SESSION['username'] = $login_user->login_session_variables()["username"];
-        header("Location: pages/profile-page.php");
+    if ($complete_form) {
+        if ($password_match) {
+            session_start();
+            $_SESSION['id'] = $login_user->login_session_variables()["id"];
+            $_SESSION['username'] = $login_user->login_session_variables()["username"];
+            header("Location: pages/profile-page.php");
+        }
+        else {
+            header("Location: pages/error/incorrect-password.php");
+        }
     }
     else {
-        header("Location: pages/error/incorrect-password.php");
+        header("Location: pages/error/incomplete-field.php");
     }
+
 
 ?>
