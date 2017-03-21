d0e1caa5464a98180aa61591a702bcb0ecf90e58
Usmaan Ali <usmaanalii@hotmail.co.uk>
2017-03-21 19:40:22 +0000

    Added meta tag for mobile viewport

--- pages/error/not-registered.php
+++ pages/error/not-registered.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="../../src/css/header.css">
         <link rel="stylesheet" href="../../src/css/center-page.css">
         <style media="screen">
