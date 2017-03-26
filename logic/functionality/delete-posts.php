9ed8acb9ba99088dfd46fded9b2d61ff1ca0d48e
Usmaan Ali <usmaanalii@hotmail.co.uk>
2017-03-26 22:14:14 +0100

    Added delete_post to SqlHelper
    - delete-posts.php now works through the SqlHelper Class
    - Trying to make as much code run via OOP as possible

--- logic/functionality/delete-posts.php
+++ logic/functionality/delete-posts.php
@@ -1,19 +1,10 @@
 <?php
-require_once __DIR__  . '/../../database-connection.php';
+require_once __DIR__  . '/../../includes/sql-helper.inc.php';
 
 if ($_POST) {
-    function delete_post($post_time)
-    {
+    $sql_helper = new SqlHelper();
 
-        global $db_connection;
-        $statement = $db_connection->prepare("DELETE FROM posts WHERE time = ?");
-
-        $statement->bind_param("s", $post_time);
-        $statement->execute();
-        $statement->close();
-    }
-
-    delete_post($_POST['post-time']);
+    $sql_helper->delete_post($_POST['post-time']);
 }
 
 // No closing php tag according to php style guide
