64f58342d3cca5d4f360ad1090d41ca793d5b937
Usmaan Ali <usmaanalii@hotmail.co.uk>
2017-04-05 10:27:47 +0100

    Preliminary work on OMDB API

---
+++ functionality/movie-apis/omdb/search-title.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style media="screen">
+        </style>
+        <title>OMDB API</title>
+    </head>
+    <body>
+        <?php
+            // URL's
+            $search_url = "http://www.omdbapi.com/?t=";
+
+            // Parameters
+            $search_params = array(
+                'i' => 'imdb id',
+                't' => 'title',
+                'type' => 'movie, series or episode',
+                'plot' => 'short, full',
+                'r' => 'json or xml',
+                'callback' => 'JSONP callback name',
+                'v' => 'API version'
+            );
+
+            // Building a search
+
+
+            $movie = urlencode("avatar");
+
+            $movie_json = file_get_contents($search_url . $movie);
+
+            $movie_data = json_decode($movie_json);
+
+            // Keys
+            $keys = array(
+
+            );
+
+            foreach ($movie_data as $key => $value) {
+                array_push($keys, $key);
+            }
+
+            // Results
+            // print_r($movie_data);
+
+        ?>
+
+        <img src="<?php echo $movie_data->Poster; ?>" alt="">
+    </body>
+</html>
---
+++ functionality/movie-apis/omdb/search.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style media="screen">
+        </style>
+        <title>OMDB API</title>
+    </head>
+    <body>
+        <?php
+            // URL's
+            $search_url = "http://www.omdbapi.com/?s=";
+
+            // Parameters
+            $search_params = array(
+                'type' => 'movie, series or episode',
+                'y' => 'year of release',
+                'r' => 'json or xml',
+                'page' => '1-100',
+                'callback' => 'JSONP callback name',
+                'v' => 'API version'
+            );
+
+
+            $movie = urlencode("avatar");
+
+            $movie_json = file_get_contents($search_url . $movie);
+
+            $movie_data = json_decode($movie_json);
+
+            // Keys
+            $keys = array(
+
+            );
+
+            foreach ($movie_data as $key => $value) {
+                array_push($keys, $key);
+            }
+
+            // Results
+            // print_r($data_array);
+
+        ?>
+
+    </body>
+</html>
