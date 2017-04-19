0549391d1f738917cd210d7837dd1408b0d5e226
Usmaan Ali <usmaanalii@hotmail.co.uk>
2017-04-19 22:08:09 +0100

    Work on image upload
    - It might not be possible for the assignment due to permission rights
    when transferring to the local university server

---
+++ tests/logic/profile/upload-image.php
@@ -0,0 +1,4 @@
+<form action="process-image.php" method="post" enctype="multipart/form-data">
+    Your Photo: <input type="file" name="image" size="25" />
+    <input type="submit" name="submit" value="Submit" />
+</form>
