f2e419e7163c74021699d1216f1239bb0141e297
Usmaan Ali <usmaanalii@hotmail.co.uk>
2017-05-03 14:41:11 +0100

    Changed posts SASS and updated timeline logic
    - Made post text stretch longer within each post
    - Timeline posts deletion was redirecting to profiles, changed it to
    redirect to timeline

--- pages/components/timeline/posts.php
+++ pages/components/timeline/posts.php
@@ -45,7 +45,7 @@ back to the respective profile pages
                     - Form that allows the user to delete posts
                 -->
                 <?php if ($post['sender_username'] == $_GET['username'] || $post['sender_username'] == $_SESSION['username']): ?>
-                    <form class="delete-post-form" action="../logic/profile.php?username=<?php echo $_SESSION['username']; ?>" method="post">
+                    <form class="delete-post-form" action="../logic/timeline.php?username=<?php echo $_SESSION['username']; ?>" method="post">
                         <input type="hidden" name="post-recipient" value="<?php echo $post['recipient_username'] ?>">
                         <input type="hidden" name="delete-post-id" value="<?php echo $post['post_id'] ?>">
                         <input class="btn btn-danger delete-button" type="submit" name="delete-post" value="x">
